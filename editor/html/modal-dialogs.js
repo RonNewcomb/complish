00a1e4b5d3978dde510d1e0d785f33d075f424f9
Ron Newcomb <pscion@yahoo.com>
2020-09-07 14:37:13 -0700

    neaten framework and modal-dialog handling

---
+++ editor/html/modal-dialogs.js
@@ -0,0 +1,12 @@
+export const ask = (message, buttonLabels) => new Promise(async (resolve) => {
+    if (!buttonLabels || !buttonLabels.length)
+        buttonLabels = ["OK"];
+    const container = document.getElementsByTagName('modal-dialogs')[0];
+    container.insertAdjacentHTML('beforeend', `<modal-dialog message=${message}><div>${buttonLabels.map(label => `<button>${label}</button>`).join(' ')}</div></modal-dialog>`);
+    const modalDialogElement = await window.loadHtml(container.lastElementChild);
+    for (let button of modalDialogElement.getElementsByTagName('button'))
+        button.addEventListener('click', e => {
+            resolve(e.currentTarget.innerText);
+            modalDialogElement.remove();
+        });
+});
