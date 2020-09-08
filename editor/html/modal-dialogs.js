6baf7e4ee8397eb3ca6744da5e227eef0bac16ce
Ron Newcomb <pscion@yahoo.com>
2020-09-07 17:56:33 -0700

    for-each kinda works but in the modal-dialog gets multi-evaluated.

--- editor/html/modal-dialogs.js
+++ editor/html/modal-dialogs.js
@@ -2,7 +2,13 @@ export const ask = (message, buttonLabels) => new Promise(async (resolve) => {
     if (!buttonLabels || !buttonLabels.length)
         buttonLabels = ["OK"];
     const container = document.getElementsByTagName('modal-dialogs')[0];
-    container.insertAdjacentHTML('beforeend', `<modal-dialog message=${message}><div>${buttonLabels.map(label => `<button>${label}</button>`).join(' ')}</div></modal-dialog>`);
+    container.insertAdjacentHTML('beforeend', `
+        <modal-dialog message=${message}>
+            <for-each iter='${JSON.stringify(buttonLabels)}'>
+                <button>{{iter}}</button>
+            </for-each>
+        </modal-dialog>`);
+    console.log("going to ", container.lastElementChild);
     const modalDialogElement = await window.loadHtml(container.lastElementChild);
     for (let button of modalDialogElement.getElementsByTagName('button'))
         button.addEventListener('click', e => {
--- editor/html/modal-dialogs.ts
+++ editor/html/modal-dialogs.ts
@@ -10,7 +10,12 @@ export const ask = (message: string, buttonLabels?: string[]) =>
         if (!buttonLabels || !buttonLabels.length) buttonLabels = ["OK"];
 
         const container = document.getElementsByTagName('modal-dialogs')[0];
-        container.insertAdjacentHTML('beforeend', `<modal-dialog message=${message}><div>${buttonLabels.map(label => `<button>${label}</button>`).join(' ')}</div></modal-dialog>`);
+        container.insertAdjacentHTML('beforeend', `
+        <modal-dialog message=${message}>
+            <for-each iter='${JSON.stringify(buttonLabels)}'>
+                <button>{{iter}}</button>
+            </for-each>
+        </modal-dialog>`);
         const modalDialogElement = await window.loadHtml(container.lastElementChild!);
 
         for (let button of modalDialogElement.getElementsByTagName('button'))
